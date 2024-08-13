48defea3a7bbe05104ceb40aa9262bdd6f9904d8
CloudDing <1301866313@qq.com>
2024-08-13 17:14:28 +0800

    feat: 修改脚本

--- copy-verify-file.js
+++ copy-verify-file.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 fs.copyFileSync('google7e5f35787211cf47.html', 'dist/google7e5f35787211cf47.html');
-fs.copyFileSync('public/index.html', 'dist/404.html');
+fs.copyFileSync('dist/index.html', 'dist/404.html');
